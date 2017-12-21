17c434b46d8baec4a6400d2db2dd1951c08338b0
Darya Koreneva <daric2612@gmail.com>
2017-12-20 16:49:36 -0800

    pom file

--- src/test/java/logic/RunTest.java
+++
@@ -1,9 +0,0 @@
-package logic;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({CanCoverTests.class, StructureTest.class, FDTest.class})
-public class RunTest {
-}
