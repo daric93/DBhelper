50a6377d07a46d75751e19ef7cc5af15ffd6b214
Darya Koreneva <daric2612@gmail.com>
2017-12-20 16:50:49 -0800

    moved methods to Structure class

--- src/main/java/logic/FD.java
+++ src/main/java/logic/FD.java
@@ -1,95 +1,24 @@
 package logic;
 
-import org.apache.commons.lang3.ArrayUtils;
-
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
 
 public class FD {
-    private List<String> lhs;
-    private List<String> rhs;
-
-    public List<String> getLhs() {
-        return lhs;
-    }
-
-    public void setLhs(List<String> lhs) {
-        this.lhs = lhs;
-    }
-
-    public List<String> getRhs() {
-        return rhs;
-    }
-
-    public void setRhs(List<String> rhs) {
-        this.rhs = rhs;
-    }
+    private final List<String> lhs;
+    private final List<String> rhs;
 
     FD(List<String> lhs, List<String> rhs) {
         this.lhs = lhs;
         this.rhs = rhs;
     }
 
-    public void removeExtraneousLHS(Set<FD> funcDependencies) {
-        if (lhs.size() > 1) {
-            List<String[]> powerSet = getPowerSet(lhs);
-            for (String[] attr : powerSet) {
-                List<String> attributeClosure = attributeClosure(attr, funcDependencies);
-                if (attributeClosure.containsAll(rhs)) {
-                    lhs = Arrays.asList(attr);
-                }
-            }
-        }
+    public List<String> getLhs() {
+        return lhs;
     }
 
-    public void removeExtraneousRHS(Set<FD> funcDependencies) {
-        List<String> attrClsr = attributeClosure(lhs.toArray(new String[0]), removeFD(funcDependencies, this));
-        if (attrClsr.containsAll(rhs))
-            rhs = null;
-    }
-
-    public List<FD> splitRHS() {
-        List<FD> list = new ArrayList<>();
-        for (String attr : rhs)
-            list.add(new FD(lhs, Collections.singletonList(attr)));
-        return list;
-    }
-
-    private static List<String> attributeClosure(String[] attr, Set<FD> funcDependencies) {
-        List<String> attrs = new ArrayList<>();
-        attrs.addAll(Arrays.asList(attr));
-        boolean changed = true;
-        while (changed) {
-            changed = false;
-            for (FD fd : funcDependencies)
-                if (attrs.containsAll(fd.lhs)) {
-                    if (fd.rhs != null && !attrs.containsAll(fd.rhs)) {
-                        attrs.addAll(fd.rhs);
-                        changed = true;
-                    }
-                }
-        }
-        return attrs;
-    }
-
-    private Set<FD> removeFD(Set<FD> fds, FD fd) {
-        return fds.stream().filter(f -> !f.equals(fd)).collect(Collectors.toSet());
-    }
-
-    private List<String[]> getPowerSet(List<String> attr) {
-        List<String[]> sets = new ArrayList<>();
-        if (attr.isEmpty())
-            return Collections.EMPTY_LIST;
-        String currAttr = attr.get(0);
-        sets.add(new String[]{currAttr});
-        List<String[]> powerSet = getPowerSet(attr.subList(1, attr.size()));
-        for (String[] subs : powerSet) {
-            sets.add(subs);
-            String[] set = ArrayUtils.add(subs, currAttr);
-            if (set.length != attr.size())
-                sets.add(set);
-        }
-        return sets;
+    public List<String> getRhs() {
+        return rhs;
     }
 
     @Override
@@ -105,13 +34,14 @@ public class FD {
         if (this == o) return true;
         if (!(o instanceof FD)) return false;
         FD fd = (FD) o;
-        return Objects.equals(getLhs(), fd.getLhs()) &&
-                Objects.equals(getRhs(), fd.getRhs());
+        boolean b = getLhs().size() == fd.getLhs().size() && getLhs().containsAll(fd.getLhs()) &&
+                getRhs().size() == fd.getRhs().size() && getRhs().containsAll(fd.getRhs());
+        return b;
     }
 
     @Override
     public int hashCode() {
-
+        getLhs().sort(Comparator.naturalOrder());
         return Objects.hash(getLhs(), getRhs());
     }
 }
--- src/main/java/logic/Structure.java
+++ src/main/java/logic/Structure.java
@@ -1,42 +1,119 @@
 package logic;
 
-import java.util.HashSet;
-import java.util.Set;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Structure {
-    private Set<FD> funcDependencies;
 
-    Structure(Set<FD> fds) {
-        this.funcDependencies = fds;
+    public void isLossless() {
+
     }
 
-    public Set<FD> getFDs() {
-        return funcDependencies;
+    public void isDependencyPreserving() {
+
     }
 
-    public Set<FD> canonicalCover() {
-        splitRHS();
-        removeExtrLHS();
-        removeExtrRHS();
-        return funcDependencies;
+    public void firstNF() {
     }
 
-    private void removeExtrLHS() {
-        funcDependencies.forEach(fd -> fd.removeExtraneousLHS(funcDependencies));
+    public void secondNF() {
+
     }
 
-    private void removeExtrRHS() {
-        funcDependencies.forEach(fd -> fd.removeExtraneousRHS(funcDependencies));
-        funcDependencies = funcDependencies.stream()
-                .filter(fd -> fd.getRhs() != null)
+    public void thirdNF() {
+
+    }
+
+    public void bcNF() {
+
+    }
+
+    public void fourthNF() {
+
+    }
+
+    public void fifthNF() {
+
+    }
+
+    public void sixthNF() {
+
+    }
+
+
+    static Set<FD> canonicalCover(Set<FD> funcDependencies) {
+        Set<FD> fdSet = splitRHS(funcDependencies);
+        fdSet = removeExtrLHS(fdSet);
+        return removeExtrRHS(fdSet);
+    }
+
+    static Set<FD> removeExtrLHS(Set<FD> funcDependencies) {
+        Set<FD> fds = new HashSet<>();
+        funcDependencies.forEach(fd -> {
+                List<String[]> powerSet = getPowerSet(fd.getLhs());
+                for (String[] attr : powerSet) {
+                    List<String> attributeClosure = attributeClosure(attr, funcDependencies);
+                    if (attributeClosure.containsAll(fd.getRhs())) {
+                        fds.add(new FD(Arrays.asList(attr), fd.getRhs()));
+                        break;
+                    }
+                }
+        });
+        return fds;
+    }
+
+    static Set<FD> removeExtrRHS(Set<FD> funcDependencies) {
+        return funcDependencies.stream()
+                .filter(fd -> {
+                    List<String> attrClsr = attributeClosure(fd.getLhs().toArray(new String[0]), removeFD(funcDependencies, fd));
+                    return !attrClsr.containsAll(fd.getRhs());
+                })
                 .collect(Collectors.toSet());
     }
 
-    private void splitRHS() {
+    static Set<FD> splitRHS(Set<FD> funcDependencies) {
         Set<FD> fds = new HashSet<>();
         for (FD fd : funcDependencies)
-            fds.addAll(fd.splitRHS());
-        funcDependencies = fds;
+            for (String attr : fd.getRhs())
+                fds.add(new FD(fd.getLhs(), Arrays.asList(attr)));
+        return fds;
+    }
+
+    static List<String[]> getPowerSet(List<String> attr) {
+        List<String[]> sets = new ArrayList<>();
+        if (attr.isEmpty())
+            return Collections.emptyList();
+        String currAttr = attr.get(0);
+        sets.add(new String[]{currAttr});
+        List<String[]> powerSet = getPowerSet(attr.subList(1, attr.size()));
+        for (String[] subs : powerSet) {
+            sets.add(subs);
+            String[] set = ArrayUtils.add(subs, currAttr);
+                sets.add(set);
+        }
+        return sets;
+    }
+
+    static List<String> attributeClosure(String[] attr, Set<FD> funcDependencies) {
+        List<String> attrs = new ArrayList<>();
+        attrs.addAll(Arrays.asList(attr));
+        boolean changed = true;
+        while (changed) {
+            changed = false;
+            for (FD fd : funcDependencies)
+                if (attrs.containsAll(fd.getLhs())) {
+                    if (fd.getRhs() != null && !attrs.containsAll(fd.getRhs())) {
+                        attrs.addAll(fd.getRhs());
+                        changed = true;
+                    }
+                }
+        }
+        return attrs;
+    }
+
+    static Set<FD> removeFD(Set<FD> fds, FD fd) {
+        return fds.stream().filter(f -> !f.equals(fd)).collect(Collectors.toSet());
     }
 }
