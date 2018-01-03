f646c8ba4aa5bbe2505ba1f69825c166e785ea66
Darya Koreneva <daric2612@gmail.com>
2018-01-03 14:15:11 -0800

    override equals, hashcode, tostring methods

--- src/main/java/logic/Table.java
+++ src/main/java/logic/Table.java
@@ -1,5 +1,6 @@
 package logic;
 
+import java.util.Objects;
 import java.util.Set;
 
 import static com.google.common.collect.Sets.newHashSet;
@@ -17,5 +18,26 @@ public class Table {
 
     public Set<String> getAttributes() {
         return attributes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Table)) return false;
+        Table table = (Table) o;
+        return Objects.equals(getAttributes(), table.getAttributes());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getAttributes());
+    }
+
+    @Override
+    public String toString() {
+        return "Table{" +
+                "attributes=" + attributes +
+                '}';
     }
 }
